e884545997fe569b720ddeb23317df27dce6e9bf
Manoj Dixit <mndxt007@gmail.com>
2023-03-02 05:59:03 +0530

    Added documenatation and comments

--- Bots/CustomQABot.cs
+++ Bots/CustomQABot.cs
@@ -103,6 +103,8 @@ namespace Microsoft.BotBuilderSamples.Bots
                 if (response.FirstOrDefault().Context.Prompts.Any())
                 {
                     var reply = MessageFactory.Text("Select the appropriate suggestion");
+                    
+                    //This method enumerates through prompts return from QA services and adds a suggested card actions
                     activities.Add(GetPromptOptions(response, reply));
                 }
 
@@ -139,6 +141,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             }
         }
 
+//This method enumerates through prompts return from QA services and adds a suggested card actions
         protected Activity GetPromptOptions(QueryResult[] response, Activity reply)
         {
             var Actions = new List<CardAction>();
